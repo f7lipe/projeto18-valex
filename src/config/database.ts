e9089a34d779b9034e4deb0fbb9b2719de605bd3
= <f7lipe@me.com>
2022-07-11 10:35:44 -0300

    fix: start

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,10 +4,14 @@ import './setupEnv.ts'
 const { Pool } = pg;
 const databaseConfig = {
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: process.env.MODE === `PROD` ? false : true,
-  },
+  ssl: {},
 };
+
+if (process.env.MODE === "PROD") {
+  databaseConfig.ssl = {
+    rejectUnauthorized: false,
+  };
+}
 
 const connection = new Pool(databaseConfig)
 
